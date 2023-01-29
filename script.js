4a46ce341030537c91212212af4b8b9cfb5ed7f1
Alberto Romero <aromero7764@gmail.com>
2023-01-29 11:50:24 -0400

    primeri commit

---
+++ script.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+    // Verifica si la cookie existe
+    var color = getCookie("color");
+    if (color == "") {
+      // Si no existe, la crea con un valor por defecto
+      color = "lightgray";
+      setCookie("color", color, 30);
+    }
+    // Ajusta el color de fondo del div
+    document.getElementById("cuadro").style.backgroundColor = color;
+  };
+  
+  // Función para crear una cookie
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  // Función para leer una cookie
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
